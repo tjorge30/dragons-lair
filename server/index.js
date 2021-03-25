db40b3f3db8911f5c813aad3b211fe8c4264c7d3
tjorge30 <mrs.javalera@gmail.com>
2021-03-25 15:29:43 -0600

    completed

---
+++ server/index.js
@@ -0,0 +1,43 @@
+
+require('dotenv').config();
+const express = require('express');
+const session = require ('express-session');
+const massive = require('massive');
+const authCtrl = requre('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
+
+const PORT = 4000;
+
+const { SESSION_SECRET, CONNECTION_STRING } =  process.env;
+
+const app = express();
+
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set('db', db);
+    console.log('bd connected');
+});
+
+app.use(
+    session({
+        resave: true,
+        saveuninitialized: false,
+        secret: SESSION_SECRET,
+    })
+);
+
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
